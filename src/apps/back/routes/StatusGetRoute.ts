92a0d9d31a26ac51cfc95144b95bf7118cc83a8e
rjurado <rjurado@nosolosoftware.es>
2022-06-16 14:50:58 +0200

    Fix import routes + added .tool-versions

--- src/apps/back/routes/StatusGetRoute.ts
+++ src/apps/back/routes/StatusGetRoute.ts
@@ -3,12 +3,10 @@ import { Request, Response, Router } from 'express'
 import { StatusGetController } from '../controllers/StatusGetController'
 import { Route } from './Route'
 
-const route: Route = {
+export const route: Route = {
   register: (router: Router) => {
     const controller = new StatusGetController
 
     router.get('/status', (req: Request, res: Response) => controller.run(req, res))
   }
 }
-
-export const { register } = route
--- src/apps/back/routes/index.ts
+++ src/apps/back/routes/index.ts
@@ -2,12 +2,12 @@ import { Router } from 'express'
 import glob from 'glob'
 
 export function registerRoutes(router: Router) {
-  const routes = glob.sync(__dirname + '/*Route.ts')
+  const routes = glob.sync(__dirname + '/?*Route.ts')
+  console.log(routes)
   routes.map(route => register(route, router))
 }
 
 function register(routePath: string, router: Router) {
-  const route = require(routePath)
-  console.log(route)
+  const { route } = require(routePath)
   route.register(router)
 }
